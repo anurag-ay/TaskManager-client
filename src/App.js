7a558ad2b21c64476e12ddd5a0c6ffc17e8d8f8a
anurag-ay <ay1909069@gmail.com>
2023-12-17 16:31:42 +0530

    add context consumption

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import Home from "./pages/Home";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import LogIn from "./pages/LogIn";
+import NotFound from "./pages/NotFound";
+import UserInfoProvider from "./context/userInfoContext";
+import UserTaskProvider from "./context/userTaskContext";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <UserInfoProvider>
+              <UserTaskProvider>
+                <Home />
+              </UserTaskProvider>
+            </UserInfoProvider>
+          }
+        />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
