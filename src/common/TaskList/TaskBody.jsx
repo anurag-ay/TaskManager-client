c49d5947ead63364bfead045baeeb251d5c694bd
anurag-ay <ay1909069@gmail.com>
2023-12-19 20:05:29 +0530

    Add tasks

--- src/common/TaskList/TaskBody.jsx
+++ src/common/TaskList/TaskBody.jsx
@@ -1,25 +1,15 @@
 import { Stack } from "@mui/material";
 import React from "react";
 import Task from "./Task";
+import { useRenderTask } from "../../context/renderTasksContext";
 
 function TaskBody() {
+  const [renderTask] = useRenderTask();
+
   return (
     <Stack gap={1} sx={{ height: "62vh", overflowY: "scroll" }}>
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-      <Task />
+      {renderTask &&
+        renderTask.map((item, index) => <Task key={index} task={item} />)}
     </Stack>
   );
 }
