c83bc4396b5e582a2e763ea4f5448fafd7299ea2
anurag-ay <ay1909069@gmail.com>
2023-12-20 00:42:29 +0530

    Add back button in nav Bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,46 @@
-import { DoneOutlineSharp } from "@mui/icons-material";
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import { ArrowCircleLeft, DoneOutlineSharp } from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Stack,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import React from "react";
 
 function NavBar() {
+  const theme = useTheme();
+  const query = useMediaQuery(theme.breakpoints.down("sm"));
   return (
     <Box>
       <AppBar sx={{ backgroundColor: "#212121" }} position="static">
         <Toolbar variant="dense">
-          <IconButton sx={{ color: "#4aaaef" }}>
-            <DoneOutlineSharp sx={{ height: "0.9em", width: "0.9em" }} />
-          </IconButton>
-          <Typography sx={{ ml: "0.3em", fontSize: "0.9em" }}>
-            Task Manager
-          </Typography>
+          <Stack
+            direction="row"
+            sx={{
+              alignItems: "center",
+              justifyContent: "space-between",
+              flexGrow: "1",
+            }}
+          >
+            <Stack direction="row" sx={{ alignItems: "center" }}>
+              <IconButton sx={{ color: "#4aaaef" }}>
+                <DoneOutlineSharp sx={{ height: "0.9em", width: "0.9em" }} />
+              </IconButton>
+              <Typography sx={{ ml: "0.3em", fontSize: "0.9em" }}>
+                Task Manager
+              </Typography>
+            </Stack>
+
+            {query && (
+              <IconButton sx={{ color: "white" }}>
+                <ArrowCircleLeft />
+              </IconButton>
+            )}
+          </Stack>
         </Toolbar>
       </AppBar>
     </Box>
