4a86e213c325260e1de53478676ab8eefacb2c8c
anurag-ay <ay1909069@gmail.com>
2023-12-19 22:34:19 +0530

    Remove unused variable

--- src/common/ImportantCategory.jsx
+++ src/common/ImportantCategory.jsx
@@ -1,7 +1,7 @@
 import { IconButton, Stack, Typography } from "@mui/material";
 import React from "react";
 import { useActiveCategory } from "../context/activeCategoryContext";
-import { Cloud, Stars } from "@mui/icons-material";
+import { Stars } from "@mui/icons-material";
 
 function ImportantCategory() {
   const [, setActiveCategory] = useActiveCategory();
