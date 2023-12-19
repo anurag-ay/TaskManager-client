a036a89615d04f0a6a9448a055eb8759030e1644
anurag-ay <ay1909069@gmail.com>
2023-12-20 01:10:00 +0530

    add responsive context and consumed it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,20 +4,25 @@ import RightSideBar from "../components/RightSideBar";
 import TaskList from "../components/TaskList";
 // import LeftSideBar from "../components/LeftSideBar";
 import { Grid, useMediaQuery, useTheme } from "@mui/material";
+import { useResponsive } from "../context/responsiveContext";
 
 function Home() {
   const theme = useTheme();
   const query = useMediaQuery(theme.breakpoints.up("sm"));
+  const [isRes] = useResponsive();
+
   return (
     <>
       <NavBar />
       <Grid container sx={{ height: "93.4vh", boxSizing: "border-box" }}>
-        <Grid item xs={12} sm={3} lg={3}>
-          <RightSideBar />
-        </Grid>
+        {!isRes && (
+          <Grid item xs={12} md={3} sm={3} lg={3}>
+            <RightSideBar />
+          </Grid>
+        )}
 
-        {query && (
-          <Grid item sm lg xl>
+        {(isRes || query) && (
+          <Grid item flexGrow="1">
             <TaskList />
           </Grid>
         )}
