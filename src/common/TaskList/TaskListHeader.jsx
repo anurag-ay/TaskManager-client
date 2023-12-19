94a3c015dc61a23b711d4621f92cbfa7354f7ee1
anurag-ay <ay1909069@gmail.com>
2023-12-19 20:57:17 +0530

    Add display data dynamically in task header

--- src/common/TaskList/TaskListHeader.jsx
+++ src/common/TaskList/TaskListHeader.jsx
@@ -1,23 +1,84 @@
-import { DoneTwoTone } from "@mui/icons-material";
-import { IconButton, Stack, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { Sort } from "@mui/icons-material";
+import {
+  IconButton,
+  Menu,
+  MenuItem,
+  Stack,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { useActiveCategory } from "../../context/activeCategoryContext";
+import { useUserCategory } from "../../context/userCategoryContext";
+import { useUserInfo } from "../../context/userInfoContext";
 
 function TaskListHeader() {
+  const [activeCategory] = useActiveCategory();
+  const [categories] = useUserCategory();
+  const userInfo = useUserInfo();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  function currentDate() {
+    let today = new Date();
+    return today.toLocaleDateString("en-US", {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+    });
+  }
+
+  function currentCategory() {
+    if (activeCategory === "today") {
+      return "Today";
+    } else if (activeCategory === "important") {
+      return "Important";
+    } else if (activeCategory === "done") {
+      return "Done";
+    } else if (activeCategory === userInfo?.allTaskCategory) {
+      return "Todo";
+    } else {
+      return categories?.find((category) => category._id === activeCategory)
+        ?.type;
+    }
+  }
+
   return (
     <Stack sx={{ mb: "1em" }}>
       <Stack
         direction="row"
         sx={{ justifyContent: "space-between", color: "white" }}
       >
-        <Typography variant="h1" sx={{ fontSize: "2em" }}>
-          Category Name
+        <Typography
+          variant="h1"
+          sx={{ fontSize: "2em", color: "black", fontWeight: "700" }}
+        >
+          {currentCategory()}
         </Typography>
-        <IconButton>
-          <DoneTwoTone />
-        </IconButton>
+
+        <Tooltip title="Sort">
+          <IconButton onClick={handleClick} sx={{ color: "black" }}>
+            <Sort />
+          </IconButton>
+        </Tooltip>
+
+        <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+          <MenuItem>Latest</MenuItem>
+          <MenuItem>Oldest</MenuItem>
+        </Menu>
       </Stack>
+
       <Typography variant="body1" color="white">
-        Friday, December 15
+        {currentDate()}
       </Typography>
     </Stack>
   );
