abfef7bc6e8fc9c09a4d81376f787caf9496459a
anurag-ay <ay1909069@gmail.com>
2023-12-19 20:04:03 +0530

    Add category

--- src/common/Category.jsx
+++ src/common/Category.jsx
@@ -1,18 +1,63 @@
-import { Task } from "@mui/icons-material";
-import { Box, Typography } from "@mui/material";
+import { Delete, Task } from "@mui/icons-material";
+import { IconButton, Stack, Typography } from "@mui/material";
 import React from "react";
+import axios, { deleteCategoryRoute } from "../api/api";
+import { useUserInfo } from "../context/userInfoContext";
+import { useUserCategory } from "../context/userCategoryContext";
+import { useActiveCategory } from "../context/activeCategoryContext";
+import { useRenderTask } from "../context/renderTasksContext";
+import { useUserTasks } from "../context/userTaskContext";
 
-function Category() {
+function Category({ category }) {
+  const { type } = category;
+  const userInfo = useUserInfo();
+  const [categories, setUserCategory] = useUserCategory();
+  const [activeCategory, setActiveCategory] = useActiveCategory();
+  const [, setRenderTask] = useRenderTask();
+  const [userTasks, setUserTasks] = useUserTasks();
+
+  async function deleteCategory() {
+    if (!userInfo) return;
+    if (!category) return;
+
+    try {
+      const res = await axios.delete(
+        `${deleteCategoryRoute}/${userInfo._id}/${category._id}`
+      );
+      const deletedCategory = res.data;
+
+      const newCategoryList = categories.filter(
+        (category) => category._id !== deletedCategory._id
+      );
+
+      const taskListAfterCategoryDeletion = userTasks.filter(
+        (task) => task.category !== deletedCategory._id
+      );
+      setUserTasks(taskListAfterCategoryDeletion);
+
+      setActiveCategory("");
+      setUserCategory(newCategoryList);
+      setRenderTask([]);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function selectCategory() {
+    setActiveCategory(category._id);
+  }
+
   return (
     <>
-      <Box
+      <Stack
+        onClick={selectCategory}
+        direction="row"
         sx={{
-          height: "1.8em",
-          display: "flex",
-          alignItems: "center",
-          backgroundColor: "#232323",
-          padding: "0.3em",
+          backgroundColor:
+            activeCategory === category?._id ? "#6f6f6f" : "#232323",
+          padding: "0.7em",
           borderRadius: "0.2em",
+          justifyContent: "space-between",
           "&:hover": {
             boxShadow: "0 0 1 1",
             cursor: "pointer",
@@ -21,18 +66,27 @@ function Category() {
           color: "white",
         }}
       >
-        <Box
+        <Stack
+          direction="row"
+          spacing={1}
           sx={{
-            display: "flex",
             alignItems: "center",
             color: "white",
-            gap: "0.5em",
           }}
         >
           <Task />
-          <Typography>Category</Typography>
-        </Box>
-      </Box>
+          <Typography>{type}</Typography>
+        </Stack>
+
+        <IconButton
+          sx={{
+            color: "white",
+          }}
+          onClick={deleteCategory}
+        >
+          <Delete />
+        </IconButton>
+      </Stack>
     </>
   );
 }
