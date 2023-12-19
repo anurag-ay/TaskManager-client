1fccdb139376575b069098f38cf4405313a287ec
anurag-ay <ay1909069@gmail.com>
2023-12-19 20:06:24 +0530

    Fix home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,21 +2,25 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import RightSideBar from "../components/RightSideBar";
 import TaskList from "../components/TaskList";
-import LeftSideBar from "../components/LeftSideBar";
-import { Grid } from "@mui/material";
+// import LeftSideBar from "../components/LeftSideBar";
+import { Grid, useMediaQuery, useTheme } from "@mui/material";
 
 function Home() {
+  const theme = useTheme();
+  const query = useMediaQuery(theme.breakpoints.up("sm"));
   return (
     <>
       <NavBar />
-
       <Grid container sx={{ height: "93.4vh", boxSizing: "border-box" }}>
-        <Grid item lg={2}>
+        <Grid item xs={12} sm={3} lg={3}>
           <RightSideBar />
         </Grid>
-        <Grid item xs sm lg xl>
-          <TaskList />
-        </Grid>
+
+        {query && (
+          <Grid item sm lg xl>
+            <TaskList />
+          </Grid>
+        )}
         {/* <Grid item lg={2}>
           <LeftSideBar />
         </Grid> */}
