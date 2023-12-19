d7614356a3c8db985e6f457101a617a6e19121c1
anurag-ay <ay1909069@gmail.com>
2023-12-19 23:46:06 +0530

    Fix typo and handle isCheck

--- src/common/TaskList/Task.jsx
+++ src/common/TaskList/Task.jsx
@@ -10,7 +10,7 @@ import axios, { deleteTaskRoute, updateTaskRoute } from "../../api/api";
 import { useUserInfo } from "../../context/userInfoContext";
 import { useRenderTask } from "../../context/renderTasksContext";
 import { useUserTasks } from "../../context/userTaskContext";
-import formatDate from "../../utils/fromatDate";
+import formatDate from "../../utils/formatDate";
 
 function Task({ task }) {
   const [importantTask, setImportantTask] = useState(false);
@@ -127,7 +127,11 @@ function Task({ task }) {
         }}
       >
         <Stack direction="row" sx={{ alignItems: "center" }}>
-          <Checkbox sx={{ color: "white" }} onChange={handleTaskStatusChange} />
+          <Checkbox
+            sx={{ color: "white" }}
+            onChange={handleTaskStatusChange}
+            checked={isDone}
+          />
           <Typography ml="0.6em">{isDone ? <s>{title}</s> : title}</Typography>
         </Stack>
 
