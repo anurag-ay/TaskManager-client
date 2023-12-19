b0bd2d4c6ca47a44eecaf6b066fa1ce74c02ea52
anurag-ay <ay1909069@gmail.com>
2023-12-19 20:03:51 +0530

    Update all tasks

--- src/common/AllTaskCategory.jsx
+++ src/common/AllTaskCategory.jsx
@@ -1,15 +1,30 @@
 import { Stack, Typography } from "@mui/material";
 import React from "react";
+import { useActiveCategory } from "../context/activeCategoryContext";
+import { useUserInfo } from "../context/userInfoContext";
 
 function AllTaskCategory() {
+  const [activeCategory, setActiveCategory] = useActiveCategory();
+  const userInfo = useUserInfo();
+
+  function handleClick() {
+    if (!userInfo) return;
+    setActiveCategory(userInfo.allTaskCategory);
+  }
+
   return (
     <>
       <Stack
+        onClick={handleClick}
         direction="row"
         sx={{
-          backgroundColor: "#232323",
+          backgroundColor:
+            activeCategory === userInfo?.allTaskCategory
+              ? "#6f6f6f"
+              : "#232323",
           padding: "0.7em",
           borderRadius: "0.2em",
+          justifyContent: "space-between",
           "&:hover": {
             boxShadow: "0 0 1 1",
             cursor: "pointer",
@@ -26,7 +41,7 @@ function AllTaskCategory() {
             color: "white",
           }}
         >
-          <Typography>All Tasks</Typography>
+          <Typography>To Do</Typography>
         </Stack>
       </Stack>
     </>
