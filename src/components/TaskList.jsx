7631d7fe06a36dbd8c993af3e574bd6b217dee35
anurag-ay <ay1909069@gmail.com>
2023-12-20 15:18:07 +0530

    add progress bar

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,14 +1,16 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, LinearProgress, Stack } from "@mui/material";
 import React, { useRef } from "react";
 import backgroundImage from "../assets/backgroundImages/Background.jpg";
 import TaskListHeader from "../common/TaskList/TaskListHeader";
 import TaskBody from "../common/TaskList/TaskBody";
 import TaskListFooter from "../common/TaskList/TaskListFooter";
 import { useActiveCategory } from "../context/activeCategoryContext";
+import { useIsProgress } from "../context/isProgressContext";
 
 function TaskList() {
   const [activeCategory] = useActiveCategory();
   const taskBodyRef = useRef();
+  const [isProgress] = useIsProgress();
 
   function isSpecificCategory() {
     if (
@@ -21,30 +23,38 @@ function TaskList() {
     return true;
   }
   return (
-    <Box
-      sx={{
-        height: "93.4vh",
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        p: "2em",
-        boxSizing: "border-box",
-      }}
-    >
-      <Grid container direction="column">
-        <Grid item>
-          <TaskListHeader />
-        </Grid>
-        <Grid item>
-          <TaskBody taskBodyRef={taskBodyRef} />
-        </Grid>
-        {isSpecificCategory() && (
+    <>
+      {isProgress && (
+        <Stack sx={{ width: "100%", color: "grey.500" }} spacing={2}>
+          <LinearProgress color="success" />
+        </Stack>
+      )}
+
+      <Box
+        sx={{
+          height: "93.4vh",
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          p: "2em",
+          boxSizing: "border-box",
+        }}
+      >
+        <Grid container direction="column">
           <Grid item>
-            <TaskListFooter taskBodyRef={taskBodyRef} />
+            <TaskListHeader />
           </Grid>
-        )}
-      </Grid>
-    </Box>
+          <Grid item>
+            <TaskBody taskBodyRef={taskBodyRef} />
+          </Grid>
+          {isSpecificCategory() && (
+            <Grid item>
+              <TaskListFooter taskBodyRef={taskBodyRef} />
+            </Grid>
+          )}
+        </Grid>
+      </Box>
+    </>
   );
 }
 
